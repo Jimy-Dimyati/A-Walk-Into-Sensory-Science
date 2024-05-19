f6959c1dcd58a4ddff898907f951aaf806b505c1
Jimy-Dimyati <113796534+Jimy-Dimyati@users.noreply.github.com>
2024-05-20 01:05:46 +0700

    Mengenal Data - Part 1.r

---
+++ Mengenal Data - Part 1.r
@@ -0,0 +1,14 @@
+library(readr)
+
+chocolates <- 
+  read_csv(
+    "https://storage.googleapis.com/dqlab-dataset/chocolates.csv",
+    col_types = cols(
+      panelist = col_factor(),
+      session = col_factor(),
+      rank = col_factor(),
+      product = col_factor(levels = paste0("choc", 1:6)),
+      .default = col_integer()
+    )
+  )
+chocolates
